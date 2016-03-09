68d21a88baaf58c773cdbcb0cdc7021dffa0f769
marco <usechermarcoj@gmail.com>
2016-03-09 16:45:15 -0430

    ejemplo de modificacion

--- main.cpp
+++ main.cpp
@@ -23,6 +23,8 @@ int main(int argc, char** argv) {
 	srand(time(0));
 	system("color 6");
     int mp[f][c], vec[5], nivel=1;
+    printf("\tDigger.");
+    system("cls");
     
     construir(mp, nivel);
     imprimir( mp);
